8624bee2f5f616acd47a116004070e6a2a4443b1
The Creator <nb3slayer@gmail.com>
2018-05-29 00:06:55 +0200

    push

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,9 @@
 # Default configuration file
 if [ ! -f /config/qBittorrent.conf ]
 then
-	cp /default/qBittorrent.conf /config/qBittorrent.conf
+	cp /default/qBittorrent.conf /config/qBittorrent.conf && \
+	chown -R qbittorrent:qbittorrent /config /torrents /qbit-downloads && \
+	chmod -R 0775 /config /torrents /qbit-downloads
 fi
 
 # Allow groups to change files.
